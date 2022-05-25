59e80f5d9c18a930ba93c5236e584cef31a4c59a
Erik-EFL <erik.lima.f@outlook.com>
2022-05-25 12:29:45 -0300

    Adicionando requisito 13

--- desafio13.sql
+++ desafio13.sql
@@ -1,2 +1,3 @@
 /* Mostre o supplier_id das purchase_orders em que o supplier_id seja 1 ou 3. */
-select supplier_id from purchase_orders where supplier_id in (1,3);
+
+SELECT supplier_id FROM purchase_orders WHERE supplier_id IN (1,3);
