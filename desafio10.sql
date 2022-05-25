0e8597977ba09b7575c7d15613541cba835c885e
Erik-EFL <erik.lima.f@outlook.com>
2022-05-25 12:29:27 -0300

    Adicionando requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,5 +1,4 @@
 /* 10 - Mostre todos os dados da tabela purchase_orders em ordem decrescente, ordenados por created_by em que o created_by é maior ou igual a 3.
-  👀 Observações técnicas
 Como critério de desempate para a ordenação, ordene também os resultados pelo id de forma crescente. */
 
 SELECT * FROM northwind.purchase_orders
