8203788e8ec968c693cd7d22f9f7b454c25332ea
Erik-EFL <erik.lima.f@outlook.com>
2022-05-25 12:28:42 -0300

    Adicionando requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,3 @@
-/* 5 - Monte uma query que exiba os dados da tabela products a partir do quarto registro até o décimo terceiro.
-  👀 Observações técnicas
-Tanto o quarto quanto o décimo terceiro registros, precisam aparecer na consulta;
-Não use where ou order by. */
+/* 5 - Monte uma query que exiba os dados da tabela products a partir do quarto registro até o décimo terceiro.*/
 
 SELECT * FROM northwind.products LIMIT 10 OFFSET 3;
