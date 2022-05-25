8e14dd1cb57dea1496898be803782beca266ff03
Erik-EFL <erik.lima.f@outlook.com>
2022-05-25 01:27:24 -0300

    Adicionando comentarios :rocket:

--- desafio5.sql
+++ desafio5.sql
@@ -1,1 +1,6 @@
+/* 5 - Monte uma query que exiba os dados da tabela products a partir do quarto registro até o décimo terceiro.
+  👀 Observações técnicas
+Tanto o quarto quanto o décimo terceiro registros, precisam aparecer na consulta;
+Não use where ou order by. */
+
 SELECT * FROM northwind.products LIMIT 10 OFFSET 3;
--- desafio7.sql
+++ desafio7.sql
@@ -1,2 +1,2 @@
-SELECT id from northwind.products
-order by id desc limit 5;
+/* 7 - Mostre apenas os ids dos 5 últimos registros da tabela products (a ordernação deve ser baseada na coluna id). */
+SELECT id from northwind.products order by id desc limit 5;
